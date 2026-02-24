9b81f12179ba348e52032690f3a5256748a30880
Tobias Koppers <tobias.koppers@googlemail.com>
2026-02-24 15:45:57 +0100

    Pass IssueSource to esm_resolve in importModule handler
    Provide the processed source file as the issue source for the
    esm_resolve call in the ImportModule handler, consistent with
    all other issue sources in the file.

--- turbopack/crates/turbopack-node/src/transforms/webpack.rs
+++ turbopack/crates/turbopack-node/src/transforms/webpack.rs
@@ -665,7 +665,7 @@ impl EvaluateContext for WebpackLoaderContext {
                     request_vc,
                     EcmaScriptModulesReferenceSubType::ImportModule,
                     ResolveErrorMode::Error,
-                    None,
+                    Some(IssueSource::from_source_only(self.context_source_for_issue)),
                 )
                 .await?;
 
