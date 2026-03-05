7de36c176563bdcf04d38c89dc484e704dba8dae
Tobias Koppers <tobias.koppers@googlemail.com>
2026-03-05 18:55:54 +0100

    Fix build: add featureNamesToMask to wasm stub and generated-native types

--- packages/next/src/build/swc/index.ts
+++ packages/next/src/build/swc/index.ts
@@ -1345,6 +1345,11 @@ async function loadWasm(importPath = '') {
             '`css.lightning.transformStyleAttr` is not supported by the wasm bindings.'
           )
         },
+        featureNamesToMask: function (_names: string[]) {
+          throw new Error(
+            '`css.lightning.featureNamesToMask` is not supported by the wasm bindings.'
+          )
+        },
       },
     },
     isWasm: true,
